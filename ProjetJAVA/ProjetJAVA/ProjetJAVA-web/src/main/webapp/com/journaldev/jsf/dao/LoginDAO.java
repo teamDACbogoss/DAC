5ff3e07d8c41ecee8fece43e3ae51eb76b101491
toto <kevin.bonkoski@ensimag.grenoble-inp.fr>
2016-03-02 18:16:37 +0100

    ajout boutons

--- ProjetJAVA/ProjetJAVA/ProjetJAVA-web/src/main/webapp/com/journaldev/jsf/dao/LoginDAO.java
+++ ProjetJAVA/ProjetJAVA/ProjetJAVA-web/src/main/webapp/com/journaldev/jsf/dao/LoginDAO.java
@@ -15,7 +15,7 @@ public class LoginDAO {
  
         try {
             con = DataConnect.getConnection();
-            ps = con.prepareStatement("Select uname, password from Users where uname = ? and password = ?");
+            ps = con.prepareStatement("Select email, password from User where email = ? and password = ?");
             ps.setString(1, user);
             ps.setString(2, password);
  
