da2ecfb6c12091696336ce8d0b836aa0432c3c53
Baptiste Josi <baptiste.josi@ensimag.imag.fr>
2016-03-23 10:11:06 +0100

    login ok

--- ProjetJAVA/ProjetJAVA/ProjetJAVA-web/src/main/java/fr/ensimag/projetjava/servlet/login.java
+++ ProjetJAVA/ProjetJAVA/ProjetJAVA-web/src/main/java/fr/ensimag/projetjava/servlet/login.java
@@ -8,6 +8,7 @@ package fr.ensimag.projetjava.servlet;
 import javax.inject.Named;
 import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -38,12 +39,21 @@ public class login implements Serializable {
         if (email == null || pwd == null) {
             msg = "Incorrect Username and Password";
             return "login";
-        }
-        else if (email.equals("test") && pwd.equals("test")) {
-            return "portfolio";
         } else {
-            msg = "Incorrect Username and Password";
-            return "login";
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            sessionBean session = (sessionBean)facesContext.getApplication()
+                    .createValueBinding("#{sessionBean}").getValue(facesContext);
+            if (email.equals("test") && pwd.equals("test")) {
+                return session.login(email, false);
+            } else if (email.equals("admin") && pwd.equals("admin")) {
+                return session.login(email, true);
+            } else {
+                msg = "Incorrect Username and Password";
+                return "login";
+            }
         }
+       
+        
+        
     }
 }
